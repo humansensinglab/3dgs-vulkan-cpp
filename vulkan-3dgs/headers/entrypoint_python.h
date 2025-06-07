4eac47d066b7c913f7bd86269bbc972adc335a76
Alejandro <gobberishch@gmail.com>
2025-06-07 04:38:58 -0400

    pybind as submodule

--- vulkan-3dgs/headers/entrypoint_python.h
+++ vulkan-3dgs/headers/entrypoint_python.h
@@ -1,6 +1,6 @@
 #pragma once
 #include "string"
-
+#include <optional>
 void renderGaussians(const std::string path,
                      std::optional<uint32_t> width = std::nullopt,
                      std::optional<uint32_t> height = std::nullopt);
