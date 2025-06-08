44da369601acfd151a852981b8c1b6a067e582d8
Alejandro <gobberishch@gmail.com>
2025-06-08 04:15:44 -0400

    .sh compile shaders

--- vulkan-3dgs/src/scripts/compile_shaders.sh
+++ vulkan-3dgs/src/scripts/compile_shaders.sh
@@ -11,6 +11,8 @@ glslangValidator -V --target-env spirv1.5 ../Shaders/idkeys.comp -o ../Shaders/i
 glslangValidator -V --target-env spirv1.3 ../Shaders/tile_boundaries.comp -o ../Shaders/boundaries.spv
 glslangValidator -V --target-env spirv1.5 ../Shaders/render_shared_mem.comp -o ../Shaders/render_shared.spv
 glslangValidator -V --target-env spirv1.5 ../Shaders/upsample.comp -o ../Shaders/upsample.spv
+glslangValidator -V --target-env spirv1.5 ../Shaders/vertex_axis.vert -o ../Shaders/axis_vert.spv
+glslangValidator -V --target-env spirv1.5 ../Shaders/frag_axis.frag -o ../Shaders/axis_frag.spv
 
 # Radix sort shaders (need APPLE define for MoltenVK compatibility)
 glslangValidator -V --target-env spirv1.5 -DAPPLE ../Shaders/radix_sort/radixsort.comp -o ../Shaders/sort.spv
